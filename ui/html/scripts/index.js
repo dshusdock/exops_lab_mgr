fb804825e701a0f791f9324693b4e985d89b274c
dshusdock <david.shusdock@ipc.com>
2024-08-29 18:54:34 +0000

    moved this

--- ui/html/scripts/index.js
+++ ui/html/scripts/index.js
@@ -147,23 +147,6 @@ document.addEventListener("alpine:init", () => {
         }
       },
     })),
-    Alpine.data('tblehdr', () => ({
-      onHdrClick(event) {
-        let modal = document.getElementsByClassName("tbl-hdr-modal")[0];
-        modal.style.display = "flex";
-        modal.style.left = event.clientX - 250 + "px";
-        modal.style.top = event.clientY - 100 + "px";
-        let modalText = document.getElementsByClassName(
-          "tbl-hdr-modal__text"
-        )[0];
-        modalText.innerText = event.target.innerText;
-      },
-      onCloseClick(event) {
-        console.log("close clicked");
-        let modal = document.getElementsByClassName("tbl-hdr-modal")[0];
-        modal.style.display = "none";
-      },
-    })),
     Alpine.data('sidenav', () => ({
       list_search: "",
       chevronRotated: false,
